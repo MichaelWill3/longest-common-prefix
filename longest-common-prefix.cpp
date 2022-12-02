e662da4dcb384d2772a26e2afb0baf964c6edfeb
MichaelWill <michaelwill3@gmail.com>
2022-12-02 18:39:26 +0100

    Pass 3rd test

--- longest-common-prefix.cpp
+++ longest-common-prefix.cpp
@@ -5,15 +5,33 @@
 class solution
 {
     public:
-    static std::string longestCommonPrefix(const std::vector<std::string>& strs)
+    static std::string longestCommonPrefix(const std::vector<std::string>& inputVector)
     {
-        return "fl";
+        std::string commonPrefix = inputVector[0];
+        size_t commonPrefixLength = inputVector[0].length();
+        for(std::string inputString : inputVector)
+        {
+            commonPrefixLength = std::min(commonPrefixLength,inputString.length());
+            bool abruptBreak = false;
+            for(size_t charIndex = 0; charIndex < commonPrefixLength; charIndex++)
+            {
+                if(inputString.at(charIndex) != commonPrefix.at(charIndex)) 
+                { 
+                    commonPrefixLength = charIndex; 
+                    abruptBreak = true; 
+                    break;
+                }
+            }
+            if(commonPrefixLength == 0) { break; }
+        }
+        return commonPrefix.substr(0,commonPrefixLength);
     }
 };
 
 int main()
 {
-    std::vector<std::string> case1 = {"flower","flow","flight"};
-    assert(solution::longestCommonPrefix(case1).compare("fl") == 0 );
+    assert(solution::longestCommonPrefix({"flower","flow","flight"}).compare("fl") == 0 );
+    assert(solution::longestCommonPrefix({"dog","racecar","car"}).compare("") == 0 );
+    assert(solution::longestCommonPrefix({"ab","a"}).compare("a") == 0 );
     return 0;
 }
